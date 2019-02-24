2e0f70b853bdec3349735f7b81be658bbc035177
piromanser <piromanseix@gmail.com>
2019-02-24 13:15:56 +0300

    add second slider

--- js/main.js
+++ js/main.js
@@ -1,11 +1,57 @@
-const glide = new Glide('.glide', {
-  type: 'carousel',
-  startAt: 0,
-  perView: 2,
-  peek: {
-    before: 300,
-    after: 300,
-  }
+document.addEventListener('DOMContentLoaded', () => {
+  (() => {
+    const peek = () => Math.round((window.innerWidth * 0.2) / 2)
+  
+    const glide = new Glide('.glide-one', {
+      type: 'carousel',
+      startAt: 0,
+      perView: 2,
+      peek: peek(),
+      // autoplay: 4000,
+      breakpoints: {
+        960: {
+          perView: 1
+        },
+      }
+    })
+    
+    const update = () => {
+      glide.update({
+        peek: peek()
+      })
+    }
+    
+    let timeout
+    const delay = 250
+    
+    window.addEventListener('resize', () => {
+      clearTimeout(timeout)
+      timeout = setTimeout(update, delay)
+    })
+    
+    glide.mount()  
+  })();
+
+  (() => {
+    const glide = new Glide('.glide-two', {
+      type: 'carousel',
+      startAt: 0,
+      gap: 30,
+      perView: 7,
+      breakpoints: {
+        1600: {
+          perView: 4
+        },
+        960: {
+          perView: 2
+        },
+        640: {
+          perView: 1
+        }
+      }
+    })
+
+    glide.mount()
+  })();
 })
 
-glide.mount()
